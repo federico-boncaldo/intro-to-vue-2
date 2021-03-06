78bb33ecc58fbe23dbe8349a55842d688dbf0b0f
Federico Boncaldo <federico@cryptolab.net>
2021-03-06 18:54:01 +0000

    Add code for: 4 - List rendering

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        inventory: 100
+        inventory: 100,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+            }
+        ]
     }
 })
